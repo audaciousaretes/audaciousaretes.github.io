fbdfc069d71f12b43b542607d9595a4842d4fd40
Mychelle Blake <mychelle.blake@gmail.com>
2015-07-23 12:00:45 -0700

    Adds revised firstjs.js with one console log output.

--- public/firstjs.js
+++ public/firstjs.js
@@ -1,11 +1,6 @@
 for (var i = 0; i <= 100; i++) {
-	if (i >= 0 && i < 101) {
-	console.log(i);}
-	if (i % 3 === 0 && i % 5 !== 0) {
-	console.log(i + " Fizz");}
-	if (i % 5 === 0 && i % 3 !== 0) {
-	console.log(i + " Buzz");}
-	if (i % 3 === 0 && i % 5 === 0) {
-	console.log(i + " FizzBuzz");}
-}
-
+	var puts = '';
+	if ((i % 3 === 0) && (i % 5 !== 0)) puts += i + ' Fizz';
+	if ((i % 5 === 0) && (i % 3 !== 0)) puts += i + ' Buzz';
+	if ((i % 3 === 0) && (i % 5 === 0)) puts += i + ' FizzBuzz';
+	console.log(puts || i);}
