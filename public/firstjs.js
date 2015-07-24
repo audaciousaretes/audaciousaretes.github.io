6ccfd8957087fa4954c8d03be16e1697c0b81084
Bryon Volmer <bryoninvegas@gmail.com>
2015-07-23 18:26:46 -0700

    js edit

--- public/firstjs.js
+++ public/firstjs.js
@@ -1,9 +1,33 @@
-for (var i = 1; i <= 100; i++) {
-	if (i % 1 === 0){
+// first attempt
+
+var i = "1";
+for(i = 0; i <= 100; i++){
+
+	if (i % 1 <= 0){
 		console.log(i);// 1=1
 	}
-	if (i === 3){
-		console.log( i + "fizz");
+	if (i % <= 3){
+		console.log(i + "fizz");
 	}
 	if (i )
 }
+
+// second attempt
+var i = 1
+for(var i = 0; i <= 100; i++){
+
+	if(i % 1 <= 0){
+		console.log(i);// 1=1
+	}
+	if(i % 3 <= 0){
+		console.log(i + "fizz");
+	}
+	if(i % 5 <= 0){
+		console.log(i + "buzz");
+	}
+	else if(i % 15 <= 0){
+		console.log(i + "Fizzbuzz");
+}
+}
+ 
+   
